eca21f8007f1802a195dc3101984979c2e90e9bf
CHANDU-46 <chandanchandan35175@gmail.com>
2026-03-03 23:14:26 +0530

    Remove unwanted folders from repository

--- FILEMODULE/File.js
+++
@@ -1,7 +0,0 @@
-const fs= require('fs');
-
-fs.writeFileSync('hello.txt',"HELLO WORLD FILE MODULE");
-
-const data=fs.readFileSync('hello.txt');
-
-console.log("THE FILE CONTENT :  "+data);
--- MODULES/index.js
+++
@@ -1,9 +0,0 @@
-const math=require("./math");
-
-console.log("Hello the file is workinng");
-
-const res1=math.add(2,3);
-const res2=math.sub(5,3);
-
-console.log("THE RESULT OF ADDITION IS "+res1);
-console.log("THE RESULT OF SUBTRACTION IS "+res2);
